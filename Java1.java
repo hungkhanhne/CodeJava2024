183d09cc83b55939b0901ff30e667133bf710f26
hungkhanhne <hungkhanhphan834@gmail.com>
2024-08-24 18:38:02 +0700

    project

---
+++ Java1.java
@@ -0,0 +1,10 @@
+
+package java1;
+
+public class Java1 {
+    public static void main(String[] args) {
+     int x = 5;
+        System.out.println(x + x); //10 + dung de con gia tri
+        System.out.println("x" + x); // +dung de noi chuoi
+    }
+}
---
+++ j2.java
@@ -0,0 +1,11 @@
+
+package java1;
+
+public class j2 {
+    public static void main(String[] args) {
+        final double PI = 3.14;
+//        PI = PI +1; => SAI VI PI LA HANG SO
+        System.out.println("pi = " + PI);
+    }
+    
+}
